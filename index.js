70426245d87df4bc2297d8b95aab4df634f578ff
SUPRIYA GUPTA <100029756+supriya331@users.noreply.github.com>
2022-02-27 01:07:27 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,158 @@
+ //let a = 18;
+ //let b = 29;
+ //let sum = 0;
+ //sum = sum + (a + b);
+ //sum += a+b;
+ //console.log(sum);
+
+ //for(let i=1; i<10; i++) {
+    // console.log(i);
+// }
+
+//var num = 10;
+//{
+    //let num = 20;
+//}
+     
+
+//var num = 10;
+//if (num === 10) {
+    //var num = 2;
+    //console.log(num);
+//}
+//console.log(num);
+ //const num = 1;
+//num = 2;
+//if (num === 1) {
+   // const num = 20;
+    //num = 21;
+    //ifconsole.log (num);
+//}
+//console.log(num);
+
+
+//function myfunc() {
+    //var num1 = 1;
+    //let num2 = 2;
+    //const num3 = 3;
+    //{
+        //var num1 = 100;
+        //let num2 = 200;
+        //const num3 = 300;
+        //console.log ("inside block:",num1);
+        //console.log ("inside block:",num2);
+        //console.log ("inside block:",num3);
+    //}
+   // console.log ("outside block:",num1);
+   // console.log ("outside block:",num2);
+   // console.log ("outside block:",num3);
+//}
+    //myfunc();
+
+
+//let employee = {
+    //name: "supriya",
+    //age : 25,
+    //designation : "full stack developer",
+    //address: {
+        //city : "sultanpur",
+        //state : "uttarpardehs",
+        //country : "india",
+   // },
+//};
+      //let { name, age, designation} = employee;
+      //console.log('
+      ///me:${name}
+      //age:${age}
+      //designation: ${designation}
+      //city:${employee.address.city}
+      //');
+
+
+
+
+
+//class calc{
+    //static getAverage(...data){
+        //logic to get the avg of all the number
+      //  let sum = 0;
+        //for(let i = 0; i < data.length;i++){
+          //  sum += data[i];
+        //}
+        //return sum / data.length;
+    //}
+
+//}
+//let average = calc.getAverage()
+
+
+//var x =  1
+//console.log(x);
+//console.log(typeof x);
+
+//var x = 1.1;
+//console.log(x);
+//console.log(typeof x);
+
+//var x = 1;
+//x = 1.1;
+//x = '1.1';
+//x = true;
+//x = null;
+//x = undefined;
+//console.log(x);
+//console.log(typeof x);
+
+//var x = ([]);
+//x = ({});
+//x = (NaN)
+//console.log(x);
+//console.log( typeof x);
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
